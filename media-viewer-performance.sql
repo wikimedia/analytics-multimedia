f34a87878cc0e79dbd053ddeb181c3515affaa70
Mark Holmquist <mtraceur@member.fsf.org>
2014-01-08 14:27:25 -0800

    Fix typo

--- media-viewer-performance.sql
+++ media-viewer-performance.sql
@@ -1,4 +1,4 @@
-select concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2) as datestring,
+select concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) as datestring,
 		sum(case when event_action = 'image-load' then event_milliseconds else 0 end) as 'image-load-totaltime',
 		avg(case when event_action = 'image-load' then event_milliseconds else 0 end) as 'image-load-meantime',
 		std(case when event_action = 'image-load' then event_milliseconds else 0 end) as 'image-load-meantime',
