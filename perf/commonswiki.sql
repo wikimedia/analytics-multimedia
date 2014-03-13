2c766fc0310c2026d47b74564c205d7ba04d93c6
Mark Holmquist <mtraceur@member.fsf.org>
2014-03-13 11:57:33 -0700

    Use new perf schema

--- perf/commonswiki.sql
+++ perf/commonswiki.sql
@@ -1,23 +1,39 @@
 select concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) as datestring,
-		sum(case when event_action = 'image-load' then event_milliseconds else 0 end) as 'image-load-totaltime',
-		avg(case when event_action = 'image-load' then event_milliseconds else 0 end) as 'image-load-meantime',
-		std(case when event_action = 'image-load' then event_milliseconds else 0 end) as 'image-load-time-std',
+		sum(case when event_type = 'filerepoinfo' then event_total else 0 end) as 'filerepoinfo-totaltime',
+		avg(case when event_type = 'filerepoinfo' then event_total else 0 end) as 'filerepoinfo-meantime',
+		std(case when event_type = 'filerepoinfo' then event_total else 0 end) as 'filerepoinfo-time-std',
 
-		sum(case when event_action = 'image-resize' then event_milliseconds else 0 end) as 'image-resize-totaltime',
-		avg(case when event_action = 'image-resize' then event_milliseconds else 0 end) as 'image-resize-meantime',
-		std(case when event_action = 'image-resize' then event_milliseconds else 0 end) as 'image-resize-time-std',
+		sum(case when event_type = 'globalusage' then event_total else 0 end) as 'globalusage-totaltime',
+		avg(case when event_type = 'globalusage' then event_total else 0 end) as 'globalusage-meantime',
+		std(case when event_type = 'globalusage' then event_total else 0 end) as 'globalusage-time-std',
 
-		sum(case when event_action = 'metadata-fetch' then event_milliseconds else 0 end) as 'metadata-fetch-totaltime',
-		avg(case when event_action = 'metadata-fetch' then event_milliseconds else 0 end) as 'metadata-fetch-meantime',
-		std(case when event_action = 'metadata-fetch' then event_milliseconds else 0 end) as 'metadata-fetch-time-std',
+		sum(case when event_type = 'image' then event_total else 0 end) as 'image-totaltime',
+		avg(case when event_type = 'image' then event_total else 0 end) as 'image-meantime',
+		std(case when event_type = 'image' then event_total else 0 end) as 'image-time-std',
 
-		sum(case when event_action = 'gender-fetch' then event_milliseconds else 0 end) as 'gender-fetch-totaltime',
-		avg(case when event_action = 'gender-fetch' then event_milliseconds else 0 end) as 'gender-fetch-meantime',
-		std(case when event_action = 'gender-fetch' then event_milliseconds else 0 end) as 'gender-fetch-time-std'
+		sum(case when event_type = 'imageinfo' then event_total else 0 end) as 'imageinfo-totaltime',
+		avg(case when event_type = 'imageinfo' then event_total else 0 end) as 'imageinfo-meantime',
+		std(case when event_type = 'imageinfo' then event_total else 0 end) as 'imageinfo-time-std'
 
-	from MediaViewerPerf_6636500
-	where event_version = '1.1'
-		and wiki = 'commonswiki'
+		sum(case when event_type = 'imageusage' then event_total else 0 end) as 'imageusage-totaltime',
+		avg(case when event_type = 'imageusage' then event_total else 0 end) as 'imageusage-meantime',
+		std(case when event_type = 'imageusage' then event_total else 0 end) as 'imageusage-time-std'
+
+		sum(case when event_type = 'thumbnailinfo' then event_total else 0 end) as 'thumbnailinfo-totaltime',
+		avg(case when event_type = 'thumbnailinfo' then event_total else 0 end) as 'thumbnailinfo-meantime',
+		std(case when event_type = 'thumbnailinfo' then event_total else 0 end) as 'thumbnailinfo-time-std'
+
+		sum(case when event_type = 'userinfo' then event_total else 0 end) as 'userinfo-totaltime',
+		avg(case when event_type = 'userinfo' then event_total else 0 end) as 'userinfo-meantime',
+		std(case when event_type = 'userinfo' then event_total else 0 end) as 'userinfo-time-std'
+
+	from (
+		select event_type, event_total, timestamp, wiki from MultimediaViewerNetworkPerformance_7393226
+			union all
+		select event_type, event_total, timestamp, wiki from MultimediaViewerNetworkPerformance_7488625
+	) as MultimediaViewerNetworkPerformanceUnioned
+
+	where wiki = 'commonswiki'
 
 	group by datestring
 	order by datestring asc;
