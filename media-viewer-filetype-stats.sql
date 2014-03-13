d0989a666326c42289da6912ed761415eeab129b
Mark Holmquist <mtraceur@member.fsf.org>
2014-03-12 17:40:01 -0700

    Remove useless things

--- media-viewer-filetype-stats.sql
+++
@@ -1,4 +0,0 @@
-select event_fileType as fileType,
-		count(*) as number_requested
-	from MediaViewerPerf_6636500
-	group by event_fileType;
--- media-viewer-image-size-stats.sql
+++
@@ -1,4 +0,0 @@
-select log(event_fileSize) as fileSizeLogScale,
-		count(*) as number_requested
-	from MediaViewerPerf_6636500
-	group by fileSizeLogScale;
