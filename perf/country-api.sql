ffd2a94511ded04f230fd4ddb4c402b26d2acff8
Gilles Dubuc <gdubuc@wikimedia.org>
2014-03-20 12:48:56 +0100

    Track per-country performance
    Queries all the data for now, when we have more data
    it'll be wiser to make it only read the last week or so

---
+++ perf/country-api.sql
@@ -0,0 +1,5 @@
+SELECT event_country as country, AVG(event_total) as mean, STD(event_total) as std, COUNT(event_total) as sample_size FROM (
+SELECT event_total, event_country, event_type FROM MultimediaViewerNetworkPerformance_7393226
+UNION ALL
+SELECT event_total, event_country, event_type FROM MultimediaViewerNetworkPerformance_7488625
+) MultimediaViewerNetworkPerformanceUnioned WHERE event_type != 'image' AND event_country != '' GROUP BY country HAVING sample_size > 10 ORDER BY mean ASC
