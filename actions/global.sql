3ad1baa06346e4107ddd0c1cf86986173e6f7862
Gilles Dubuc <gdubuc@wikimedia.org>
2014-04-15 18:27:07 +0200

    Add new actions
    This card introduced a bunch of new actions:
    
    https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/290

--- actions/global.sql
+++ actions/global.sql
@@ -1,14 +1,24 @@
 -- Get statistics, per day, for various actions in Media Viewer.
 
 SELECT CONCAT(SUBSTRING(timestamp, 1, 4), '-', SUBSTRING(timestamp, 5, 2), '-', SUBSTRING(timestamp, 7, 2)) AS datestring,
-	SUM(CASE WHEN event_action = 'thumbnail-link-click' THEN 1 ELSE 0 END) AS 'thumbnail-link-click',
-	SUM(CASE WHEN event_action = 'enlarge-link-click' THEN 1 ELSE 0 END) AS 'enlarge-link-click',
-	SUM(CASE WHEN event_action = 'fullscreen-link-click' THEN 1 ELSE 0 END) AS 'fullscreen-link-click',
-	SUM(CASE WHEN event_action = 'defullscreen-link-click' THEN 1 ELSE 0 END) AS 'defullscreen-link-click',
-	SUM(CASE WHEN event_action = 'site-link-click' THEN 1 ELSE 0 END) AS 'site-link-click',
-	SUM(CASE WHEN event_action = 'close-link-click' THEN 1 ELSE 0 END) AS 'close-link-click',
-	SUM(CASE WHEN event_action = 'use-this-file-link-click' THEN 1 ELSE 0 END) AS 'use-this-file-link-click',
-	SUM(CASE WHEN event_action = 'image-view' THEN 1 ELSE 0 END) AS 'image-view'
+	SUM(CASE WHEN event_action IN ('thumbnail-link-click', 'thumbnail') THEN 1 ELSE 0 END) AS 'thumbnail',
+	SUM(CASE WHEN event_action IN ('enlarge-link-click', 'enlarge') THEN 1 ELSE 0 END) AS 'enlarge',
+	SUM(CASE WHEN event_action IN ('fullscreen-link-click', 'fullscreen') THEN 1 ELSE 0 END) AS 'fullscreen',
+	SUM(CASE WHEN event_action IN ('defullscreen-link-click', 'defullscreen') THEN 1 ELSE 0 END) AS 'defullscreen',
+	SUM(CASE WHEN event_action IN ('close-link-click', 'close') THEN 1 ELSE 0 END) AS 'close',
+	SUM(CASE WHEN event_action IN ('site-link-click', 'file-description-page') THEN 1 ELSE 0 END) AS 'file-description-page',
+	SUM(CASE WHEN event_action IN ('use-this-file-link-click', 'use-this-file-open') THEN 1 ELSE 0 END) AS 'use-this-file-open',
+	SUM(CASE WHEN event_action = 'image-view' THEN 1 ELSE 0 END) AS 'image-view',
+	SUM(CASE WHEN event_action = 'metadata-open' THEN 1 ELSE 0 END) AS 'metadata-open',
+	SUM(CASE WHEN event_action = 'metadata-close' THEN 1 ELSE 0 END) AS 'metadata-close',
+	SUM(CASE WHEN event_action = 'next-image' THEN 1 ELSE 0 END) AS 'next-image',
+	SUM(CASE WHEN event_action = 'prev-image' THEN 1 ELSE 0 END) AS 'prev-image',
+	SUM(CASE WHEN event_action = 'terms-open' THEN 1 ELSE 0 END) AS 'terms-open',
+	SUM(CASE WHEN event_action = 'license-page' THEN 1 ELSE 0 END) AS 'license-page',
+	SUM(CASE WHEN event_action = 'author-page' THEN 1 ELSE 0 END) AS 'author-page',
+	SUM(CASE WHEN event_action = 'source-page' THEN 1 ELSE 0 END) AS 'source-page',
+	SUM(CASE WHEN event_action = 'hash-load' THEN 1 ELSE 0 END) AS 'hash-load',
+	SUM(CASE WHEN event_action = 'history-navigation' THEN 1 ELSE 0 END) AS 'history-navigation'
 
 	FROM (
 		SELECT timestamp, wiki, event_action FROM MediaViewer_7670440
@@ -19,7 +29,9 @@ SELECT CONCAT(SUBSTRING(timestamp, 1, 4), '-', SUBSTRING(timestamp, 5, 2), '-', 
 			UNION ALL
 		SELECT timestamp, wiki, event_action FROM MediaViewer_6055641
 			UNION ALL
-		SELECT timestamp, wiki, event_action FROM MediaViewer_6054199) AS MediaViewerUnioned
+		SELECT timestamp, wiki, event_action FROM MediaViewer_6054199
+			UNION ALL
+		SELECT timestamp, wiki, event_action FROM MediaViewer_8245578) AS MediaViewerUnioned
 
 	WHERE  timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY))
 
