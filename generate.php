409ef3287cdc4a3a1d2776c0081661353b8ff170
Gilles Dubuc <gdubuc@wikimedia.org>
2014-04-21 23:10:31 +0200

    Percentiles + separating data sources
    Limn graphs can have multiple data sources. Separating them
    lets us avoid doing one giant query.
    
    I'll delete the old SQL files later, once the limn interface has switched
    to these new data sources.

--- generate.php
+++ generate.php
@@ -2,7 +2,21 @@
 
 $wikis = array( 'global', 'huwiki', 'frwiki', 'enwiki', 'mediawikiwiki', 'commonswiki' );
 
-function generate( $folder, $wiki ) {
+$default_fields = 'event_type, event_total, timestamp, wiki';
+
+$perf_metrics = array(
+	array( 'name' => 'userinfo', 'where' => 'event_type = \'userinfo\'', 'fields' => $default_fields ),
+	array( 'name' => 'imageinfo', 'where' => 'event_type = \'imageinfo\'', 'fields' => $default_fields ),
+	array( 'name' => 'thumbnailinfo', 'where' => 'event_type = \'thumbnailinfo\'', 'fields' => $default_fields ),
+	array( 'name' => 'filerepoinfo', 'where' => 'event_type = \'filerepoinfo\'', 'fields' => $default_fields ),
+	array( 'name' => 'imageusage', 'where' => 'event_type = \'imageusage\'', 'fields' => $default_fields ),
+	array( 'name' => 'globalusage', 'where' => 'event_type = \'globalusage\'', 'fields' => $default_fields ),
+	array( 'name' => 'image', 'where' => 'event_type = \'image\'', 'fields' => $default_fields ),
+	array( 'name' => 'imagemiss', 'where' => 'event_type = \'image\' AND LENGTH(event_XCache) > 0 AND event_varnish1hits = 0 AND event_varnish2hits = 0 AND event_varnish2hits = 0', 'fields' => 'event_type, event_total, timestamp, wiki, event_XCache, event_varnish1hits, event_varnish2hits, event_varnish3hits' ),
+	array( 'name' => 'imagehit', 'where' => 'event_type = \'image\' AND LENGTH(event_XCache) > 0 AND (event_varnish1hits > 0 OR event_varnish2hits > 0 OR event_varnish2hits > 0)', 'fields' => 'event_type, event_total, timestamp, wiki, event_XCache, event_varnish1hits, event_varnish2hits, event_varnish3hits' ),
+	);
+
+function generate( $folder, $wiki, $metric = null ) {
 	$replacement = '';
 
 	if ( $wiki != 'global' ) {
@@ -11,11 +25,21 @@ function generate( $folder, $wiki ) {
 
 	$sql = file_get_contents( $folder . '/template.sql' );
 	$sql = str_replace( '%wiki%', $replacement, $sql );
-	file_put_contents( $folder . '/' . $wiki . '.sql', $sql );
+
+	if ( !is_null( $metric ) ) {
+		$sql = str_replace( '%metricname%', $metric['name'], $sql );
+		$sql = str_replace( '%metricfields%', $metric['fields'], $sql );
+		$sql = str_replace( '%metricwhere%', $metric['where'], $sql );
+	}
+
+	file_put_contents( $folder . '/' . $wiki . ( is_null( $metric ) ? '' : '-'  . $metric['name'] ) . '.sql', $sql );
 }
 
 foreach ( $wikis as $wiki ) {
-	generate( 'perf', $wiki );
+	foreach ( $perf_metrics as $perf_metric) {
+		generate( 'perf', $wiki, $perf_metric );
+	}
+
 	generate( 'actions', $wiki );
 }
 
--- perf/enwiki-image.sql
+++ perf/enwiki-image.sql
@@ -10,24 +10,24 @@ SELECT timestamp FROM MultimediaViewerNetworkPerformance_7917896
 LEFT OUTER JOIN
 
 ( SELECT concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) AS datestring,
-EXP(AVG(LOG(event_total))) AS %metricname%_time_mean,
-EXP(STD(LOG(event_total))) AS %metricname%_time_std,
-COUNT(*) AS %metricname%_sample_size,
+EXP(AVG(LOG(event_total))) AS image_time_mean,
+EXP(STD(LOG(event_total))) AS image_time_std,
+COUNT(*) AS image_sample_size,
 CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
   GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
-   ',', 1/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS %metricname%_time_1st_percentile,
+   ',', 1/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS image_time_1st_percentile,
 CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
   GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
-   ',', 50/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS %metricname%_time_50th_percentile,
+   ',', 50/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS image_time_50th_percentile,
 CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
   GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
-   ',', 90/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS %metricname%_time_90th_percentile,
+   ',', 90/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS image_time_90th_percentile,
 CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
   GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
-   ',', 99/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS %metricname%_time_99th_percentile FROM (
-SELECT %metricfields% FROM MultimediaViewerNetworkPerformance_7393226
+   ',', 99/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS image_time_99th_percentile FROM (
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7393226
 UNION ALL
-SELECT %metricfields% FROM MultimediaViewerNetworkPerformance_7488625
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7488625
 UNION ALL
-SELECT %metricfields% FROM MultimediaViewerNetworkPerformance_7917896
-) AS MultimediaViewerNetworkPerformanceUnioned WHERE %wiki% timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) AND %metricwhere% GROUP BY datestring ORDER BY datestring ASC ) stats USING (datestring)
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7917896
+) AS MultimediaViewerNetworkPerformanceUnioned WHERE wiki = 'enwiki' AND timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) AND event_type = 'image' GROUP BY datestring ORDER BY datestring ASC ) stats USING (datestring)
