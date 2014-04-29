3e56f9b5ebd7b14dc174f85c30f28b8e005e0483
Gilles Dubuc <gdubuc@wikimedia.org>
2014-04-29 12:47:58 +0200

    Add percentiles to the Geo data
    Change-Id: I9c4ac62fd6c2efdfbcaa31d8931988026051ec0c
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/506

--- perf/country-api.sql
+++ perf/country-api.sql
@@ -1,4 +1,21 @@
-SELECT event_country as country, EXP(AVG(LOG(event_total))) as mean, EXP(STD(LOG(event_total))) as std, COUNT(event_total) as sample_size FROM (
+SET group_concat_max_len = 10485760;
+SELECT event_country as country,
+EXP(AVG(LOG(event_total))) as mean,
+EXP(STD(LOG(event_total))) as std,
+COUNT(event_total) as sample_size,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 1/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS 1st_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 50/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS 50th_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 90/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS 90th_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 99/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS 99th_percentile
+FROM (
 SELECT timestamp, event_total, event_country, event_type FROM MultimediaViewerNetworkPerformance_7393226
 UNION ALL
 SELECT timestamp, event_total, event_country, event_type FROM MultimediaViewerNetworkPerformance_7488625
