62edd7c6693a5a585d90611af742301e2c159327
Gilles Dubuc <gdubuc@wikimedia.org>
2014-04-29 11:04:45 +0200

    Add more pilot sites
    Dutch, Japanese, Portuguese, Spanish, Swedish, Telugu, Italian, Russian
    
    Change-Id: Ic205a4bd9d62589d3fabd022816bebae71dee2ef
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/526

---
+++ perf/eswiki-imageusage.sql
@@ -0,0 +1,33 @@
+SET group_concat_max_len = 10485760;
+SELECT * FROM (SELECT concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) AS datestring FROM (
+SELECT timestamp FROM MultimediaViewerNetworkPerformance_7393226
+UNION ALL
+SELECT timestamp FROM MultimediaViewerNetworkPerformance_7488625
+UNION ALL
+SELECT timestamp FROM MultimediaViewerNetworkPerformance_7917896
+) AS MultimediaViewerNetworkPerformanceUnioned WHERE timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) GROUP BY datestring ORDER BY datestring ASC) dates
+
+LEFT OUTER JOIN
+
+( SELECT concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) AS datestring,
+EXP(AVG(LOG(event_total))) AS imageusage_time_mean,
+EXP(STD(LOG(event_total))) AS imageusage_time_std,
+COUNT(*) AS imageusage_sample_size,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 1/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS imageusage_time_1st_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 50/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS imageusage_time_50th_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 90/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS imageusage_time_90th_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 99/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS imageusage_time_99th_percentile FROM (
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7393226
+UNION ALL
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7488625
+UNION ALL
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7917896
+) AS MultimediaViewerNetworkPerformanceUnioned WHERE wiki = 'eswiki' AND timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) AND event_type = 'imageusage' GROUP BY datestring ORDER BY datestring ASC ) stats USING (datestring)
---
+++ perf/svwiki-globalusage.sql
@@ -0,0 +1,33 @@
+SET group_concat_max_len = 10485760;
+SELECT * FROM (SELECT concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) AS datestring FROM (
+SELECT timestamp FROM MultimediaViewerNetworkPerformance_7393226
+UNION ALL
+SELECT timestamp FROM MultimediaViewerNetworkPerformance_7488625
+UNION ALL
+SELECT timestamp FROM MultimediaViewerNetworkPerformance_7917896
+) AS MultimediaViewerNetworkPerformanceUnioned WHERE timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) GROUP BY datestring ORDER BY datestring ASC) dates
+
+LEFT OUTER JOIN
+
+( SELECT concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) AS datestring,
+EXP(AVG(LOG(event_total))) AS globalusage_time_mean,
+EXP(STD(LOG(event_total))) AS globalusage_time_std,
+COUNT(*) AS globalusage_sample_size,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 1/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS globalusage_time_1st_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 50/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS globalusage_time_50th_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 90/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS globalusage_time_90th_percentile,
+CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(
+  GROUP_CONCAT(event_total ORDER BY event_total SEPARATOR ','),
+   ',', 99/100 * COUNT(*) + 1), ',', -1) AS DECIMAL) AS globalusage_time_99th_percentile FROM (
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7393226
+UNION ALL
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7488625
+UNION ALL
+SELECT event_type, event_total, timestamp, wiki FROM MultimediaViewerNetworkPerformance_7917896
+) AS MultimediaViewerNetworkPerformanceUnioned WHERE wiki = 'svwiki' AND timestamp >= TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY)) AND event_type = 'globalusage' GROUP BY datestring ORDER BY datestring ASC ) stats USING (datestring)
