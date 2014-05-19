181f7c9e1a56a3a28e6cd17e84e304b52743e185
Antoine Musso <hashar@free.fr>
2014-05-19 18:13:15 +0200

    Code hygiene: pass flake8 python linter
    This patch tweaks generate.py so it pass the flake8 python linter
    available from pypi (pip install flake8).
    
    I have made it to ignore tabs (W191) and lines too long (E501).
    
    Non exhaustive list of issues fixed..
    
    Clear out dicts:
    * whitespace after '{' [E201]
    * whitespace before '}' [E202]
    * whitespace before ':' [E203]
    
    * whitespace after '(' [E201]
    * whitespace before ')' [E202]
    * multiple spaces before operator [E221]
    * statement ends with a semicolon [E703]
    * expected 2 blank lines, found 1 [E302] (before def statements)
    * unexpected spaces around keyword / parameter equals [E251]
    
    * comparison != to None should be 'if cond is not None:' [E711]
    * comparison == to None should be 'if cond is None:' [E711]
    
    * Split some very long lines
    
    Change-Id: I9d465a6f3401ab1a7b39a4e4ca8bb105b56ce548

--- generate.py
+++ generate.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
 
-wikis = ['global',
+wikis = [
+	'global',
 	'cawiki',
 	'commonswiki',
 	'czwiki',
@@ -29,54 +31,59 @@ wikis = ['global',
 	'tewiki',
 	'thwiki',
 	'viwiki',
-	]
+]
 
 default_fields = 'event_type, event_total, timestamp, wiki'
 
 perf_metrics = [
-	{ 'name' : 'userinfo', 'where' : 'event_type = \'userinfo\'', 'fields' : default_fields },
-	{ 'name' : 'imageinfo', 'where' : 'event_type = \'imageinfo\'', 'fields' : default_fields },
-	{ 'name' : 'thumbnailinfo', 'where' : 'event_type = \'thumbnailinfo\'', 'fields' : default_fields },
-	{ 'name' : 'filerepoinfo', 'where' : 'event_type = \'filerepoinfo\'', 'fields' : default_fields },
-	{ 'name' : 'imageusage', 'where' : 'event_type = \'imageusage\'', 'fields' : default_fields },
-	{ 'name' : 'globalusage', 'where' : 'event_type = \'globalusage\'', 'fields' : default_fields },
-	{ 'name' : 'image', 'where' : 'event_type = \'image\'', 'fields' : default_fields },
-	{ 'name' : 'imagemiss', 'where' : 'event_type = \'image\' AND LENGTH(event_XCache) > 0 AND event_varnish1hits = 0 AND event_varnish2hits = 0 AND event_varnish2hits = 0', 'fields' : 'event_type, event_total, timestamp, wiki, event_XCache, event_varnish1hits, event_varnish2hits, event_varnish3hits' },
-	{ 'name' : 'imagehit', 'where' : 'event_type = \'image\' AND LENGTH(event_XCache) > 0 AND (event_varnish1hits > 0 OR event_varnish2hits > 0 OR event_varnish2hits > 0)', 'fields' : 'event_type, event_total, timestamp, wiki, event_XCache, event_varnish1hits, event_varnish2hits, event_varnish3hits' },
+	{'name': 'userinfo', 'where': 'event_type = \'userinfo\'', 'fields': default_fields},
+	{'name': 'imageinfo', 'where': 'event_type = \'imageinfo\'', 'fields': default_fields},
+	{'name': 'thumbnailinfo', 'where': 'event_type = \'thumbnailinfo\'', 'fields': default_fields},
+	{'name': 'filerepoinfo', 'where': 'event_type = \'filerepoinfo\'', 'fields': default_fields},
+	{'name': 'imageusage', 'where': 'event_type = \'imageusage\'', 'fields': default_fields},
+	{'name': 'globalusage', 'where': 'event_type = \'globalusage\'', 'fields': default_fields},
+	{'name': 'image', 'where': 'event_type = \'image\'', 'fields': default_fields},
+	{'name': 'imagemiss', 'where': 'event_type = \'image\' AND LENGTH(event_XCache) > 0 AND event_varnish1hits = 0 AND event_varnish2hits = 0 AND event_varnish2hits = 0', 'fields': 'event_type, event_total, timestamp, wiki, event_XCache, event_varnish1hits, event_varnish2hits, event_varnish3hits'},
+	{'name': 'imagehit', 'where': 'event_type = \'image\' AND LENGTH(event_XCache) > 0 AND (event_varnish1hits > 0 OR event_varnish2hits > 0 OR event_varnish2hits > 0)', 'fields': 'event_type, event_total, timestamp, wiki, event_XCache, event_varnish1hits, event_varnish2hits, event_varnish3hits'},
 ]
 
 geo_perf_metrics = [
-	{ 'name' : 'api', 'where' : 'event_type != \'image\'', 'fields' : '' },
-	{ 'name' : 'image', 'where' : 'event_type = \'image\'', 'fields' : '' },
+	{'name': 'api', 'where': 'event_type != \'image\'', 'fields': ''},
+	{'name': 'image', 'where': 'event_type = \'image\'', 'fields': ''},
 ]
 
-def generate( folder, wiki, metric = None):
+
+def generate(folder, wiki, metric=None):
 	replacement = ''
 
 	if wiki != 'global':
 		replacement = 'wiki = \'' + wiki + '\' AND'
 
-	sql = open( os.path.dirname( os.path.realpath( __file__ ) ) + '/' + folder + '/template.sql' ).read( 10000 )
-	sql = sql.replace( '%wiki%', replacement )
+	sql = open(
+		os.path.dirname(os.path.realpath(__file__)) +
+		'/' + folder + '/template.sql').read(10000)
+	sql = sql.replace('%wiki%', replacement)
 
-	if metric != None:
-		sql = sql.replace( '%metricname%', metric[ 'name' ] )
-		sql = sql.replace( '%metricfields%', metric[ 'fields' ] )
-		sql = sql.replace( '%metricwhere%', metric[ 'where' ] );
+	if metric is not None:
+		sql = sql.replace('%metricname%', metric['name'])
+		sql = sql.replace('%metricfields%', metric['fields'])
+		sql = sql.replace('%metricwhere%', metric['where'])
 
-	f = open( sys.argv[ 1 ] + '/' + folder + '/'  + wiki + ( '' if metric == None else '-'  + metric[ 'name' ] ) + '.sql', 'w' )
-	f.write( sql )
+	f = open(
+		sys.argv[1] + '/' + folder + '/' +
+		wiki + ('' if metric is None else '-' + metric['name']) + '.sql', 'w')
+	f.write(sql)
 	f.close()
 
-if len( sys.argv ) != 2:
+if len(sys.argv) != 2:
 	print 'Must specific a folder to send generated SQL to'
 	sys.exit(2)
 
 for wiki in wikis:
 	for perf_metric in perf_metrics:
-		generate( 'perf', wiki, perf_metric )
+		generate('perf', wiki, perf_metric)
 
 	for geo_perf_metric in geo_perf_metrics:
-		generate( 'geoperf', wiki, geo_perf_metric )
+		generate('geoperf', wiki, geo_perf_metric)
 
-	generate( 'actions', wiki )
+	generate('actions', wiki)
