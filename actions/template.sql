e94ae0a38a5e4073d7ece064ca77e036aa45091b
Gilles Dubuc <gdubuc@wikimedia.org>
2014-06-20 09:24:11 +0200

    Calculate opt-in/opt-out totals
    Change-Id: I3557d6a1fe71d8e6f377d8ef43729c9cf8a0d467
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/675

--- actions/template.sql
+++ actions/template.sql
@@ -24,7 +24,9 @@ SELECT CONCAT(SUBSTRING(timestamp, 1, 4), '-', SUBSTRING(timestamp, 5, 2), '-', 
 	SUM(CASE WHEN event_action = 'optout-loggedin' THEN event_samplingFactor ELSE 0 END) AS 'optout-loggedin',
 	SUM(CASE WHEN event_action = 'optout-anon' THEN event_samplingFactor ELSE 0 END) AS 'optout-anon',
 	SUM(CASE WHEN event_action = 'optin-loggedin' THEN event_samplingFactor ELSE 0 END) AS 'optin-loggedin',
-	SUM(CASE WHEN event_action = 'optin-anon' THEN event_samplingFactor ELSE 0 END) AS 'optin-anon'
+	SUM(CASE WHEN event_action = 'optin-anon' THEN event_samplingFactor ELSE 0 END) AS 'optin-anon',
+	SUM(CASE WHEN event_action IN ('optout-loggedin', 'optout-anon') THEN event_samplingFactor ELSE 0 END) AS 'optout-total',
+	SUM(CASE WHEN event_action IN ('optin-loggedin', 'optin-anon') THEN event_samplingFactor ELSE 0 END) AS 'optin-total'
 
 	FROM (
 		SELECT timestamp, event_action, 1 AS event_samplingFactor FROM MediaViewer_7670440
