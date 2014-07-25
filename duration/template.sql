cce1fc27c372e0ac07dbd9aee8ad71139d637037
Gilles Dubuc <gdubuc@wikimedia.org>
2014-07-25 10:22:56 -0400

    Switch back to domComplete
    Currently we measure when the DOM becomes interactive, which
    is not what we want.
    
    Change-Id: Idd9ab04d0d18bbb96f1a87475fa142f7cbd19fad

--- duration/template.sql
+++ duration/template.sql
@@ -3,14 +3,14 @@ SET @@group_concat_max_len := @@max_allowed_packet;
 SELECT * FROM (
     SELECT
         DATE_FORMAT(timestamp, '%Y-%m-%d') AS datestring,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_50,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_90,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_95,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_99,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_50,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_90,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_95,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_99,
         COUNT(*) * 100 AS filepage_loggedin_population_size -- NavigationTiming data is sampled 1:1000
     FROM
         NavigationTiming_8365252
@@ -18,7 +18,7 @@ SELECT * FROM (
         %wiki%
         event_action = 'view'
         AND event_namespaceId = 6
-        AND event_mediaWikiLoadComplete IS NOT NULL
+        AND event_domComplete IS NOT NULL
         AND NOT event_isAnon
         AND timestamp < TIMESTAMP(CURDATE()) -- do not show partial data for the current day
         AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
@@ -27,14 +27,14 @@ SELECT * FROM (
 ) navtiming_loggedin LEFT JOIN (
     SELECT
         DATE_FORMAT(timestamp, '%Y-%m-%d') AS datestring,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_anon_50,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_anon_90,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_anon_95,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_mediaWikiLoadComplete
-            ORDER BY event_mediaWikiLoadComplete SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_anon_99,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_anon_50,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_anon_90,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_anon_95,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
+            ORDER BY event_domComplete SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_anon_99,
         COUNT(*) * 1000 AS filepage_anon_population_size
     FROM
         NavigationTiming_8365252
@@ -42,7 +42,7 @@ SELECT * FROM (
         %wiki%
         event_action = 'view'
         AND event_namespaceId = 6
-        AND event_mediaWikiLoadComplete IS NOT NULL
+        AND event_domComplete IS NOT NULL
         AND event_isAnon
         AND timestamp < TIMESTAMP(CURDATE())
         AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
