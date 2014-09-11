05f11d7342a168745a23b8dc517926fd7fc93672
Gilles Dubuc <gdubuc@wikimedia.org>
2014-09-11 09:33:07 +0200

    Flatten results to make them more limn-friendly
    Change-Id: I3e968b0218bd70f2dca366f3b8a60fe2bee00e3f
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/862

--- uw-funnel/absolute.sql
+++ uw-funnel/absolute.sql
@@ -1,11 +1,14 @@
 SELECT
     CONCAT(SUBSTRING(timestamp, 1, 4), '-', SUBSTRING(timestamp, 5, 2), '-', SUBSTRING(timestamp, 7, 2)) AS datestring,
-    event_step AS step,
-    COUNT(*) AS survivors
+    SUM(CASE WHEN event_step = 'tutorial' THEN 1 ELSE 0 END) AS 'tutorial',
+    SUM(CASE WHEN event_step = 'file' THEN 1 ELSE 0 END) AS 'file',
+    SUM(CASE WHEN event_step = 'deeds' THEN 1 ELSE 0 END) AS 'deeds',
+    SUM(CASE WHEN event_step = 'details' THEN 1 ELSE 0 END) AS 'details',
+    SUM(CASE WHEN event_step = 'thanks' THEN 1 ELSE 0 END) AS 'thanks'
 FROM
     UploadWizardStep_8851805
 WHERE timestamp >= TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 30 DAY))
 AND timestamp < TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 1 DAY))
 GROUP BY
-    datestring, event_step
+    datestring
 ORDER BY datestring ASC
