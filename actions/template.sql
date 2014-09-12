f8bc139fa91bfc4d943f8da6eab25c24e9da768f
Gilles Dubuc <gdubuc@wikimedia.org>
2014-09-12 09:25:06 +0200

    Add new schema version to action TSV
    Change-Id: Ia0af770ff6d9a116ff9299e3732dba2b28e947cc
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/878

--- actions/template.sql
+++ actions/template.sql
@@ -26,7 +26,33 @@ SELECT CONCAT(SUBSTRING(timestamp, 1, 4), '-', SUBSTRING(timestamp, 5, 2), '-', 
 	SUM(CASE WHEN event_action = 'pref-optin-loggedin' THEN event_samplingFactor ELSE 0 END) AS 'optin-loggedin',
 	SUM(CASE WHEN event_action = 'optin-anon' THEN event_samplingFactor ELSE 0 END) AS 'optin-anon',
 	SUM(CASE WHEN event_action IN ('pref-optout-loggedin', 'optout-anon') THEN event_samplingFactor ELSE 0 END) AS 'optout-total',
-	SUM(CASE WHEN event_action IN ('pref-optin-loggedin', 'optin-anon') THEN event_samplingFactor ELSE 0 END) AS 'optin-total'
+	SUM(CASE WHEN event_action IN ('pref-optin-loggedin', 'optin-anon') THEN event_samplingFactor ELSE 0 END) AS 'optin-total',
+	SUM(CASE WHEN event_action = 'about-page' THEN event_samplingFactor ELSE 0 END) AS 'about-page',
+	SUM(CASE WHEN event_action = 'discuss-page' THEN event_samplingFactor ELSE 0 END) AS 'discuss-page',
+	SUM(CASE WHEN event_action = 'help-page' THEN event_samplingFactor ELSE 0 END) AS 'help-page',
+	SUM(CASE WHEN event_action = 'location-page' THEN event_samplingFactor ELSE 0 END) AS 'location-page',
+	SUM(CASE WHEN event_action = 'uploader-page' THEN event_samplingFactor ELSE 0 END) AS 'uploader-page',
+	SUM(CASE WHEN event_action = 'download-select-menu-original' THEN event_samplingFactor ELSE 0 END) AS 'download-select-menu-original',
+	SUM(CASE WHEN event_action = 'download-select-menu-small' THEN event_samplingFactor ELSE 0 END) AS 'download-select-menu-small',
+	SUM(CASE WHEN event_action = 'download-select-menu-medium' THEN event_samplingFactor ELSE 0 END) AS 'download-select-menu-medium',
+	SUM(CASE WHEN event_action = 'download-select-menu-large' THEN event_samplingFactor ELSE 0 END) AS 'download-select-menu-large',
+	SUM(CASE WHEN event_action = 'download' THEN event_samplingFactor ELSE 0 END) AS 'download',
+	SUM(CASE WHEN event_action = 'download-view-in-browser' THEN event_samplingFactor ELSE 0 END) AS 'download-view-in-browser',
+	SUM(CASE WHEN event_action = 'share-page' THEN event_samplingFactor ELSE 0 END) AS 'share-page',
+	SUM(CASE WHEN event_action = 'share-link-copied' THEN event_samplingFactor ELSE 0 END) AS 'share-link-copied',
+	SUM(CASE WHEN event_action = 'embed-html-copied' THEN event_samplingFactor ELSE 0 END) AS 'embed-html-copied',
+	SUM(CASE WHEN event_action = 'embed-wikitext-copied' THEN event_samplingFactor ELSE 0 END) AS 'embed-wikitext-copied',
+	SUM(CASE WHEN event_action = 'embed-switched-to-html' THEN event_samplingFactor ELSE 0 END) AS 'embed-switched-to-html',
+	SUM(CASE WHEN event_action = 'embed-switched-to-wikitext' THEN event_samplingFactor ELSE 0 END) AS 'embed-switched-to-wikitext',
+	SUM(CASE WHEN event_action = 'embed-select-menu-wikitext-default' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-wikitext-default',
+	SUM(CASE WHEN event_action = 'embed-select-menu-wikitext-small' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-wikitext-small',
+	SUM(CASE WHEN event_action = 'embed-select-menu-wikitext-medium' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-wikitext-medium',
+	SUM(CASE WHEN event_action = 'embed-select-menu-wikitext-large' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-wikitext-large',
+	SUM(CASE WHEN event_action = 'embed-select-menu-html-original' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-html-original',
+	SUM(CASE WHEN event_action = 'embed-select-menu-html-small' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-html-small',
+	SUM(CASE WHEN event_action = 'embed-select-menu-html-medium' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-html-medium',
+	SUM(CASE WHEN event_action = 'embed-select-menu-html-large' THEN event_samplingFactor ELSE 0 END) AS 'embed-select-menu-html-large',
+	SUM(CASE WHEN event_action = 'use-this-file-close' THEN event_samplingFactor ELSE 0 END) AS 'use-this-file-close'
 
 	FROM (
 		SELECT timestamp, event_action, 1 AS event_samplingFactor FROM MediaViewer_7670440
@@ -39,6 +65,9 @@ SELECT CONCAT(SUBSTRING(timestamp, 1, 4), '-', SUBSTRING(timestamp, 5, 2), '-', 
 			WHERE %wiki% timestamp < TIMESTAMP(CURDATE()) AND timestamp >= TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 30 DAY))
 		UNION ALL
 		SELECT timestamp, event_action, event_samplingFactor FROM MediaViewer_8935662
+			WHERE %wiki% timestamp < TIMESTAMP(CURDATE()) AND timestamp >= TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 30 DAY))
+			UNION ALL
+		SELECT timestamp, event_action, event_samplingFactor FROM MediaViewer_9792855
 			WHERE %wiki% timestamp < TIMESTAMP(CURDATE()) AND timestamp >= TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 30 DAY))
 		UNION ALL
 		SELECT timestamp, (CASE WHEN event_value = 0 THEN 'pref-optout-loggedin' ELSE 'pref-optin-loggedin' END) AS event_action, 1 AS event_samplingFactor FROM PrefUpdate_5563398
