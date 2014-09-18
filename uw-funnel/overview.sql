ca69f3a5a5ca65239ea78eb349f000178f0af8ad
Gergő Tisza <tgr.huwiki@gmail.com>
2014-09-18 20:25:44 +0200

    Redo lost commit for overview fix
    Redo Ic92978fbce45caa0ea58ad6d8ba56fb2bf8354c7 which got
    inadvertently reverted.
    
    Change-Id: I3e474f3c51cc7c2233c214b572fa0f4d98c34a8c
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/862

--- uw-funnel/overview.sql
+++ uw-funnel/overview.sql
@@ -2,26 +2,29 @@ SET
     @survivors = NULL -- holds number of users reaching the next step
 ;
 
-SELECT
-    step,
-    @survivors / survivors AS relative,
-    @survivors := survivors AS absolute
+SELECT -- the outer select is just for sorting - putting it into the middle select messes with the variable calculation somehow
+    *
 FROM (
     SELECT
-        event_step AS step,
-        COUNT(*) AS survivors
-    FROM
-        UploadWizardStep_8851805
-    WHERE
-        timestamp >= TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 30 DAY))
-    GROUP BY
-        event_step
-    ORDER BY
-        -- we need to calculate (users reaching next step / users reaching current step)
-        -- but there is no way to know abou the next row, so we reverse the ordering
-        -- here, then reverse it again in the outer select
-        survivors ASC
-) uploadwizard_funnel_data
+        step,
+        @survivors / survivors AS relative,
+        @survivors := survivors AS absolute
+    FROM (
+        SELECT
+            event_step AS step,
+            COUNT(*) AS survivors
+        FROM
+            UploadWizardStep_8851805
+        WHERE timestamp >= TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 30 DAY))
+        GROUP BY
+            event_step
+        ORDER BY
+            -- we need to calculate (users reaching next step / users reaching current step)
+            -- but there is no way to know abou the next row, so we reverse the ordering
+            -- here, then reverse it again in the outer select
+            FIELD(step, 'tutorial', 'file', 'deeds', 'details', 'thanks') DESC
+    ) uploadwizard_funnel_data
+) steps_unsorted
 ORDER BY
-    absolute DESC
+    FIELD(step, 'tutorial', 'file', 'deeds', 'details', 'thanks') ASC
 ;
