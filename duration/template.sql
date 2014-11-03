88669fed83267f35fb17260bb3115e49dcafff26
Gilles Dubuc <gdubuc@wikimedia.org>
2014-11-03 11:51:57 +0100

    Switch duration data to ImageMetrics
    Change-Id: Id67de08146341979c521167391087e51528b00e1
    Mingle: https://wikimedia.mingle.thoughtworks.com/projects/multimedia/cards/880

--- duration/template.sql
+++ duration/template.sql
@@ -3,87 +3,47 @@ SET @@group_concat_max_len := @@max_allowed_packet;
 SELECT * FROM (
     SELECT
         DATE_FORMAT(timestamp, '%Y-%m-%d') AS datestring,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_50,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_90,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_95,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_99,
-        COUNT(*) * 100 AS filepage_loggedin_population_size -- NavigationTiming data is sampled 1:1000
-    FROM (
-        SELECT
-            *,
-            NULL event_firstPaint,
-            NULL event_firstPaintAfterLoad
-        FROM
-            NavigationTiming_8365252
-        WHERE
-            %wiki%
-            event_action = 'view'
-            AND event_namespaceId = 6
-            AND event_domComplete IS NOT NULL
-            AND NOT event_isAnon
-            AND timestamp < TIMESTAMP(CURDATE()) -- do not show partial data for the current day
-            AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
-        UNION ALL
-        SELECT
-            *
-        FROM
-            NavigationTiming_8477778
-        WHERE
-            %wiki%
-            event_action = 'view'
-            AND event_namespaceId = 6
-            AND event_domComplete IS NOT NULL
-            AND NOT event_isAnon
-            AND timestamp < TIMESTAMP(CURDATE()) -- do not show partial data for the current day
-            AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
-    ) NavigationTiming_unioned
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_50,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_90,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_95,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_loggedin_99,
+        COUNT(*) * event_samplingFactor AS filepage_loggedin_population_size
+    FROM
+        ImageMetricsLoadingTime_10078363
+    WHERE
+        %wiki%
+        event_navigationType = 'navigate'
+        AND event_fullLoadingTime IS NOT NULL
+        AND NOT event_isAnon
+        AND timestamp < TIMESTAMP(CURDATE()) -- do not show partial data for the current day
+        AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
     GROUP BY
         datestring
 ) navtiming_loggedin LEFT JOIN (
     SELECT
         DATE_FORMAT(timestamp, '%Y-%m-%d') AS datestring,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_anon_50,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_anon_90,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_anon_95,
-        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_domComplete
-            ORDER BY event_domComplete SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_anon_99,
-        COUNT(*) * 1000 AS filepage_anon_population_size
-    FROM (
-        SELECT
-            *,
-            NULL event_firstPaint,
-            NULL event_firstPaintAfterLoad
-        FROM
-            NavigationTiming_8365252
-        WHERE
-            %wiki%
-            event_action = 'view'
-            AND event_namespaceId = 6
-            AND event_domComplete IS NOT NULL
-            AND event_isAnon
-            AND timestamp < TIMESTAMP(CURDATE())
-            AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
-        UNION ALL
-        SELECT
-            *
-        FROM
-            NavigationTiming_8477778
-        WHERE
-            %wiki%
-            event_action = 'view'
-            AND event_namespaceId = 6
-            AND event_domComplete IS NOT NULL
-            AND event_isAnon
-            AND timestamp < TIMESTAMP(CURDATE())
-            AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
-        ) NavigationTiming_unioned_2
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 50/100*COUNT(*)+1), ',', -1) AS filepage_anon_50,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 90/100*COUNT(*)+1), ',', -1) AS filepage_anon_90,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 95/100*COUNT(*)+1), ',', -1) AS filepage_anon_95,
+        SUBSTRING_INDEX(SUBSTRING_INDEX(GROUP_CONCAT(event_fullLoadingTime
+            ORDER BY event_fullLoadingTime SEPARATOR ','), ',', 99/100*COUNT(*)+1), ',', -1) AS filepage_anon_99,
+        COUNT(*) * event_samplingFactor AS filepage_anon_population_size
+    FROM
+        ImageMetricsLoadingTime_10078363
+    WHERE
+        %wiki%
+        event_navigationType = 'navigate'
+        AND event_fullLoadingTime IS NOT NULL
+        AND event_isAnon
+        AND timestamp < TIMESTAMP(CURDATE()) -- do not show partial data for the current day
+        AND timestamp > TIMESTAMP(CURDATE() - INTERVAL 90 DAY)
     GROUP BY
         datestring
 ) navtiming_anon USING (datestring) LEFT JOIN (
