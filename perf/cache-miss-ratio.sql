145b53fb9925efa96c019494054a9654cac6d400
Gergő Tisza <tgr.huwiki@gmail.com>
2014-12-14 00:04:59 -0800

    Calculate image cache miss ratio
    Bug: T78205
    Change-Id: I08c41774c555778c03fbfaa9c2625dbbb01f7013

---
+++ perf/cache-miss-ratio.sql
@@ -0,0 +1,29 @@
+SELECT
+    datestring,
+    SUM(miss) / COUNT(*) miss_ratio,
+    COUNT(*) *1000 total
+FROM (
+    SELECT
+        (event_varnish1hits = 0 AND event_varnish2hits = 0 AND event_varnish3hits = 0) miss,
+        concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) datestring
+    FROM
+        MultimediaViewerNetworkPerformance_7917896
+    WHERE
+        -- %wiki%
+        event_type = 'image'
+        AND LENGTH(event_XCache) > 0
+        AND timestamp BETWEEN TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 90 DAY)) AND TIMESTAMP(CURDATE())
+UNION ALL
+    SELECT
+        (event_varnish1hits = 0 AND event_varnish2hits = 0 AND event_varnish3hits = 0) miss,
+        concat(substring(timestamp, 1, 4), '-', substring(timestamp, 5, 2), '-', substring(timestamp, 7, 2)) datestring
+    FROM
+        MultimediaViewerNetworkPerformance_10596581
+    WHERE
+        -- %wiki%
+        event_type = 'image'
+        AND LENGTH(event_XCache) > 0
+        AND timestamp BETWEEN TIMESTAMP(DATE_SUB(CURDATE(), INTERVAL 90 DAY)) AND TIMESTAMP(CURDATE())
+) MultimediaViewerNetworkPerformanceUnioned
+GROUP BY datestring
+ORDER BY datestring ASC;
